c4b45dce0f9f605c512d5863069612fffed621b5
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-16 13:11:05 -0700

    modified the styling of the pokecards

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -3,10 +3,10 @@ import './Pokecard.css';
 const Pokecard = (props) => {
   return (
     <div className="Pokecard">
-      <h4>{props.info.name}</h4>
+      <h4 className="Pokecard-name">{props.info.name}</h4>
       <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.info.id}.png`} />
-      <h5>Type: {props.info.type}</h5>
-      <h5>EXP: {props.info.base_experience}</h5>
+      <h5 className="Pokecard-type">Type: {props.info.type}</h5>
+      <h5 className="Pokecard-exp">EXP: {props.info.base_experience}</h5>
     </div>
   )
 };
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,10 +1,13 @@
 import Pokecard from './Pokecard';
 import pokemonArr from './pokemon';
+import './Pokedex.css';
 
 const Pokedex = ({ pokemon=pokemonArr }) => (
     <div className="Pokedex">
       <h1>Pokedex</h1>
-      {pokemon.map(obj => <Pokecard key={obj.id} info={obj} />)}
+      <div className="Pokedex-cards-container">
+        {pokemon.map(obj => <Pokecard key={obj.id} info={obj} />)}
+      </div>
     </div>
 );
 
