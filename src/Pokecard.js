1af42549e8afd3da291ba0ae4f9f670b5e880ba7
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-16 13:05:55 -0700

    styled the Pokecard component

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,10 +1,12 @@
+import './Pokecard.css';
+
 const Pokecard = (props) => {
   return (
     <div className="Pokecard">
-      <h5>{props.info.name}</h5>
+      <h4>{props.info.name}</h4>
       <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.info.id}.png`} />
-      <h6>Type: {props.info.type}</h6>
-      <h6>EXP: {props.info.base_experience}</h6>
+      <h5>Type: {props.info.type}</h5>
+      <h5>EXP: {props.info.base_experience}</h5>
     </div>
   )
 };
